84d0878ac7f9ff7b719039a004e352ec309d64b9
Sina Pinto <sina.pinto@gmail.com>
2015-01-16 23:24:12 -0800

    structural rework, added fullscreen

--- bwm.c
+++ bwm.c
@@ -5,7 +5,6 @@
 #include <xcb/xcb.h>
 
 /*#define DEBUG*/
-/*#define MOUSE*/
 
 #ifdef DEBUG
 #include "events.h"
@@ -28,24 +27,34 @@ typedef struct Key {
     int x, y;
 } Key;
 
+typedef struct client client;
+struct client{
+    client *next;
+    client *prev;
+    xcb_window_t win;
+};
+
 xcb_connection_t *conn;   /* connection to X server */
 xcb_screen_t *screen;     /* current screen */
-xcb_drawable_t focuswin;  /* currently focused window */
+static client *head;
+static client *current;
+static int screen_height;
+static int screen_width;
 
 static void cleanup();
 static void killwin();
 static void nextwin();
 static void resize(int x, int y);
 static void move(int x, int y);
-static void setfocus(xcb_window_t w);
+static void fullscreen();
+static void add_window(xcb_window_t w);
+static void remove_window(xcb_window_t w);
+static void focus_current();
 static void handle_events(void);
-static void grab_keys_and_buttons(void);
+static void grab_keys(void);
 
 #include "config.h"
 
-/*
- *Set keyboard focus to follow mouse pointer, then disconnect
- */
 static void cleanup()
 {
     xcb_set_input_focus(conn, XCB_NONE, XCB_INPUT_FOCUS_POINTER_ROOT,
@@ -58,144 +67,194 @@ static void cleanup()
     exit(0);
 }
 
-/*
- *Kill focuswin (used for keybind)
- */
 static void killwin()
 {
-    /* don't kill root */
-    if(!focuswin || focuswin == screen->root)
-        return;
-
-    xcb_kill_client(conn, focuswin);
-}
-
-/*
- *Change focus to the next window
- *alt + tab doesn't loop
- */
-static void nextwin()
-{
-    xcb_query_tree_reply_t *r;
-    xcb_window_t *c,t = 0;
-    xcb_get_window_attributes_cookie_t ac;
-    xcb_get_window_attributes_reply_t *ar;
-    int i;
-
-    r = xcb_query_tree_reply(conn, xcb_query_tree(conn, screen->root), 0);
-    if (!r || !r->children_len)
-        return;
-    c = xcb_query_tree_children(r);
-
-    for (i=0; i < r->children_len; i++)
+    client *c;
+    for (c=head; c != NULL; c=c->next)
     {
-        ac = xcb_get_window_attributes(conn, c[i]);
-        ar = xcb_get_window_attributes_reply(conn, ac, NULL);
-
-        if (ar)
+        if (c == current)
         {
-            if (ar->map_state == XCB_MAP_STATE_VIEWABLE) {
-                if (ar->override_redirect) break;
-                if (r->children_len == 1) {
-                    t = c[i];
-                    break;
-                }
-                if (c[i] == focuswin) break;
-                t = c[i];
-            }
+            /* don't kill root */
+            if (!c->win || c->win == screen->root)
+                return;
+
+            xcb_kill_client(conn, c->win);
+            xcb_flush(conn); 
+            return;
         }
     }
+}
 
-    if (t)
-    {
-        setfocus(t);
-    }
+static void nextwin()
+{
+  client *c;
+   if(current != NULL && head != NULL)
+   {
+        if(current->next == NULL)
+            c = head;
+        else
+            c = current->next;
+
+        current = c;
+        focus_current();
+   }
+}
+
+static void resize(int x, int y)
+{
+    /* don't resize root */
+    if (!current->win || current->win == screen->root)
+        return;
+
+    xcb_get_geometry_cookie_t c = xcb_get_geometry(conn, current->win);
+    xcb_get_geometry_reply_t *r = xcb_get_geometry_reply(conn, c, NULL);
+
+    uint32_t values[2];
+    /*don't resize to less than 5 pixels */
+    values[0] = r->width+x>5  ? r->width + x  : r->width;
+    values[1] = r->height+y>5 ? r->height + y : r->height;
+
+    PDEBUG("X: %d -> %d, Y:%d -> %d\n", r->width, r->width + x,
+                                        r->height, r->height + y);
+
+    xcb_configure_window(conn, current->win, XCB_CONFIG_WINDOW_WIDTH
+                                   | XCB_CONFIG_WINDOW_HEIGHT, values);
+    xcb_flush(conn); 
+
     free(r);
 }
 
-/*
- *Resize the focused window by x and y
- */
-static void resize(int x, int y)
-{
-    uint32_t values[2];
-    xcb_get_geometry_reply_t *geom;
-    xcb_window_t win = focuswin;
-
-    /* don't resize root */
-    if (!win || win == screen->root)
-        return;
-
-    geom = xcb_get_geometry_reply(conn, xcb_get_geometry(conn, win), NULL);
-    if (!geom)
-        return;
-
-    /*don't resize to smaller than 10px*/
-    values[0] = geom->width+x>10  ? geom->width + x  : geom->width;
-    values[1] = geom->height+y>10 ? geom->height + y : geom->height;
-
-    PDEBUG("X: %d -> %d, Y:%d -> %d\n", geom->width, geom->width + x,
-                                        geom->height, geom->height + y);
-
-    xcb_configure_window(conn, win, XCB_CONFIG_WINDOW_WIDTH
-                                   | XCB_CONFIG_WINDOW_HEIGHT, values);
-
-    free(geom);
-}
-
-/*
- *Move the focused window by x or y
- */
 static void move(int x, int y)
+
 {
-    uint32_t values[2];
-    xcb_get_geometry_reply_t *geom;
-
     /* don't move root */
-    if (!focuswin || focuswin == screen->root)
+    if (!current->win || current->win == screen->root)
         return;
 
-    geom = xcb_get_geometry_reply(conn, xcb_get_geometry(conn, focuswin), NULL);
-    if (!geom)
-        return;
+    xcb_get_geometry_cookie_t c = xcb_get_geometry(conn, current->win);
+    xcb_get_geometry_reply_t *r = xcb_get_geometry_reply(conn, c, NULL);
 
-    values[0] = geom->x + x;
-    values[1] = geom->y + y;
+    uint32_t values[2];
+    values[0] = r->x + x;
+    values[1] = r->y + y;
 
-    PDEBUG("X: %d -> %d, Y:%d -> %d\n", geom->x, geom->x + x,
-                                        geom->y, geom->y + y);
+    PDEBUG("X: %d -> %d, Y:%d -> %d\n", r->x, r->x + x,
+                                        r->y, r->y + y);
 
-    xcb_configure_window(conn, focuswin, XCB_CONFIG_WINDOW_X
+    xcb_configure_window(conn, current->win, XCB_CONFIG_WINDOW_X
                                    | XCB_CONFIG_WINDOW_Y, values);
+    xcb_flush(conn); 
 
-    free(geom);
+    free(r);
 }
 
-/*
- *unfocus focuswin and set focus to w
- */
-static void setfocus(xcb_window_t w)
+static void fullscreen()
 {
-    uint32_t values[1];
-
-    /* don't focus root */
-    if (w == screen->root || !w)
+    client *c;
+    for (c=head; c != NULL; c=c->next)
     {
-        return;
+        if (c == current)
+        {
+            uint32_t val[5];
+            uint32_t mask = XCB_CONFIG_WINDOW_X
+                          | XCB_CONFIG_WINDOW_Y
+                          | XCB_CONFIG_WINDOW_WIDTH
+                          | XCB_CONFIG_WINDOW_HEIGHT
+                          | XCB_CONFIG_WINDOW_STACK_MODE;
+            val[0] = 0;
+            val[1] = 0;
+            val[2] = screen_width;
+            val[3] = screen_height;
+            val[4] = XCB_STACK_MODE_ABOVE;
+
+            xcb_configure_window(conn, c->win, mask, val);
+        }
+    }
+    xcb_flush(conn);
+}
+
+static void add_window(xcb_window_t w) {
+    client *c,*t;
+
+    if ((c = malloc(sizeof(c))) == NULL)
+    {
+        fprintf(stderr, "Error calloc!");
+        exit(1);
     }
 
-    values[0] = NORMCOLOR;
-    xcb_change_window_attributes(conn, w, XCB_CW_BORDER_PIXEL, values);
+    if (head == NULL)
+    {
+        c->next = NULL;
+        c->prev = NULL;
+        c->win = w;
+        head = c;
+    }
+    else
+    {
+        for (t=head; t->prev != NULL; t=t->prev)
+            ;
+        c->prev = NULL;
+        c->next = t;
+        c->win = w;
+        t->prev = c;
+        head = c;
+    }
 
-    xcb_flush(conn);
+    current = c;
+}
 
-    /*unfocus the last window*/
-    values[0] = SELCOLOR;
-    xcb_change_window_attributes(conn, focuswin, XCB_CW_BORDER_PIXEL, values);
+static void remove_window(xcb_window_t w) {
+    client *c;
 
-    xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT, w, XCB_CURRENT_TIME);
+    for(c=head; c != NULL; c=c->next) {
+        if(c->win == w) {
+            if(c->prev == NULL && c->next == NULL) { // one client in list
+                free(head);
+                head = NULL;
+                current = NULL;
+                return;
+            }
+            if(c->prev == NULL) {               // head
+                head = c->next;
+                c->next->prev = NULL;
+                current = c->next;
+            }
+            else if(c->next == NULL) {          // tail
+                c->prev->next = NULL;
+                current = c->prev;
+            }
+            else {
+                c->prev->next = c->next;
+                c->next->prev = c->prev;
+                current = c->prev;
+            }
 
-    focuswin = w;
+            free(c);
+            return;
+        }
+    }
+    xcb_flush(conn); 
+}
+
+static void focus_current() {
+    client *c;
+    uint32_t values[1];
+    for (c=head; c != NULL; c=c->next)
+    {
+        if(current == c)
+        {
+            values[0] = SELCOLOR;
+            xcb_change_window_attributes(conn, c->win, XCB_CW_BORDER_PIXEL, values);
+            xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT, c->win, XCB_CURRENT_TIME);
+            xcb_flush(conn);
+        }
+        else
+        {
+            values[0] = NORMCOLOR;
+            xcb_change_window_attributes(conn, c->win, XCB_CW_BORDER_PIXEL, values);
+            xcb_flush(conn);
+        }
+    }
 }
 
 static void handle_events(void)
@@ -213,46 +272,45 @@ static void handle_events(void)
     /* block until we receive an event */
     while ( (ev = xcb_wait_for_event(conn)) )
     {
+        /* CREATE_NOTIFY -> CONFIGURE_NOTIFY -> MAP_NOTIFY */ 
         PDEBUG("Event: %s\n", evnames[ev->response_type]);
 
         switch ( ev->response_type & ~0x80 ) 
         {
-        case XCB_MAP_REQUEST:
-        {
-            xcb_map_request_event_t *e;
-            /*PDEBUG("event: map requeset\n");*/
-
-            e = (xcb_map_request_event_t *)ev;
-            xcb_map_window(conn, e->window);
-        }
-        break;
-
-        case XCB_DESTROY_NOTIFY:
-        {
-            xcb_destroy_notify_event_t *e;
-            /*PDEBUG("event: destroy window\n");*/
-
-            e = (xcb_destroy_notify_event_t *)ev;
-            xcb_kill_client(conn, e->window);
-        }
-        break;
-
         case XCB_CREATE_NOTIFY:
         {
             xcb_create_notify_event_t *e;
-            /*PDEBUG("event: create window\n");*/
-
             e = (xcb_create_notify_event_t *)ev;
 
             values[0] = XCB_EVENT_MASK_ENTER_WINDOW;
             xcb_change_window_attributes_checked(conn, e->window, 
                                                 XCB_CW_EVENT_MASK, values);
 
-            values[0] = 4;
+            values[0] = BORDER_WIDTH;
             xcb_configure_window(conn, e->window, 
                                 XCB_CONFIG_WINDOW_BORDER_WIDTH, values);
 
-            setfocus(e->window);
+            add_window(e->window);
+            focus_current();
+            xcb_flush(conn); 
+        }
+        break;
+
+        case XCB_DESTROY_NOTIFY:
+        {
+            xcb_destroy_notify_event_t *e;
+            e = (xcb_destroy_notify_event_t *)ev;
+
+            client *c;
+            for (c=head; c != NULL; c=c->next)
+            {
+                if(e->window == c->win)
+                {
+                    remove_window(e->window);
+                    focus_current();
+                    break;
+                }
+            }
         }
         break;
 
@@ -260,66 +318,40 @@ static void handle_events(void)
         {
             xcb_key_press_event_t *e;
             e = (xcb_key_press_event_t *)ev;
-            /*PDEBUG("Key %d pressed \n", e->detail);*/
+            PDEBUG("Key %d pressed \n", e->detail);
 
             int i;
-            for (i=0; i< LENGTH(keys); i++)
+            for (i=0; i<LENGTH(keys); i++)
             {
-                PDEBUG("config key = %d , key pressed = %d\n", 
-                        keys[i].keysym, e->detail);
+                /*PDEBUG("config key = %d , key pressed = %d\n", */
+                        /*keys[i].keysym, e->detail);*/
 
                 if (keys[i].keysym == e->detail
-                        && (keys[i].mod & ~0x80) == (e->state & ~0x80)
-                        && keys[i].function)
+                    && (keys[i].mod & ~0x80) == (e->state & ~0x80)
+                    && keys[i].function)
                 {
                     keys[i].function(keys[i].x, keys[i].y);
+                    xcb_flush(conn); 
                     break;
                 }
             }
         }
         break;
 
-#ifdef MOUSE
-        case XCB_ENTER_NOTIFY:
-        {
-            xcb_enter_notify_event_t *e;
-            e = (xcb_enter_notify_event_t *)ev;
-        
-            setfocus(e->event);
-        }
-        break;
-#endif
-
         } /* switch */
-
-        xcb_flush(conn); 
         free(ev);
-    } /* while */
+    }
 }
 
-/*
- *Set up keys
- */
-static void grab_keys_and_buttons(void)
+static void grab_keys(void)
 {
     int i;
 
-    for (i=0; i<LENGTH(keys); ++i)
+    for (i=0; i<LENGTH(keys); i++)
     {
         xcb_grab_key(conn, 1, screen->root, keys[i].mod, keys[i].keysym, 
                      XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
     }
-
-#ifdef MOUSE
-    xcb_grab_button(conn, 0, screen->root, XCB_EVENT_MASK_BUTTON_PRESS | 
-                XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC, 
-                XCB_GRAB_MODE_ASYNC, screen->root, XCB_NONE, 1, XCB_MOD_MASK_1);
-
-    xcb_grab_button(conn, 0, screen->root, XCB_EVENT_MASK_BUTTON_PRESS | 
-                XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC, 
-                XCB_GRAB_MODE_ASYNC, screen->root, XCB_NONE, 3, XCB_MOD_MASK_1);
-#endif
-
     xcb_flush(conn);
 }
 
@@ -338,11 +370,23 @@ int main ()
 
     /* get the first screen */
     screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
-    focuswin = screen->root;
+    xcb_drawable_t root = screen->root;
 
-    grab_keys_and_buttons();
+    screen_width = screen->width_in_pixels-(2*BORDER_WIDTH);
+    screen_height = screen->height_in_pixels-(2*BORDER_WIDTH);
+    head = NULL;
+    current = NULL;
+
+    uint32_t mask = XCB_CW_EVENT_MASK;
+    uint32_t values[1] = {XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT
+                       | XCB_EVENT_MASK_STRUCTURE_NOTIFY
+                       | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY};
+    xcb_change_window_attributes_checked(conn, root, mask, values);
+    xcb_flush(conn);
+
+    grab_keys();
 
     handle_events();
 
-    exit(1);
+    exit(0);
 }
