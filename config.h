2ce33326aa2bfe73e5f6f60c896675383f7ef1e2
Sina Pinto <sina.pinto@gmail.com>
2015-01-16 23:22:33 -0800

    new keybind

--- config.h
+++ config.h
@@ -1,26 +1,29 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
-#define MOD     XCB_MOD_MASK_1 /* find using `xmodmap`*/
+/* find using `xmodmap`*/
+#define MOD     XCB_MOD_MASK_1
 #define SHIFT   XCB_MOD_MASK_SHIFT
 
-#define NORMCOLOR    0xafaf87  /* normal window border color */
-#define SELCOLOR     0x002933  /* selected window border color */
-#define BORDER_WIDTH 4
+#define NORMCOLOR    0x002933  /* unfocused window border color */
+#define SELCOLOR     0xafaf87  /* focused window border color */
+#define BORDER_WIDTH 4         /* border width in pixels */
 
+/* find keysyms using `xev` */
 static Key keys[] = {
-   /* MOD               KEYSYM          FUNCTION    X    Y     */
-    { MOD,              44, /*j*/       move,       0,   50    },
-    { MOD,              45, /*k*/       move,       0,  -50    },
-    { MOD,              43, /*h*/       move,     -50,   0     },
-    { MOD,              46, /*l*/       move,      50,   0     },
-    { MOD | SHIFT,      44, /*j*/       resize,     0,   50    },
-    { MOD | SHIFT,      45, /*k*/       resize,     0,  -50    },
-    { MOD | SHIFT,      43, /*h*/       resize,   -50,   0     },
-    { MOD | SHIFT,      46, /*l*/       resize,    50,   0     },
-    { MOD,              23, /*Tab*/     nextwin,    0,   0     },
-    { MOD | SHIFT,      24, /*q*/       killwin,    0,   0     },
-    { MOD | SHIFT,      26, /*e*/       cleanup,    0,   0     },
+   /* MOD               KEYSYM          FUNCTION       X    Y     */
+    { MOD,              44, /*j*/       move,          0,   50    },
+    { MOD,              45, /*k*/       move,          0,  -50    },
+    { MOD,              43, /*h*/       move,        -50,   0     },
+    { MOD,              46, /*l*/       move,         50,   0     },
+    { MOD | SHIFT,      44, /*j*/       resize,        0,   50    },
+    { MOD | SHIFT,      45, /*k*/       resize,        0,  -50    },
+    { MOD | SHIFT,      43, /*h*/       resize,      -50,   0     },
+    { MOD | SHIFT,      46, /*l*/       resize,       50,   0     },
+    { MOD,              38, /*a*/       fullscreen,    0,   0     },
+    { MOD,              23, /*Tab*/     nextwin,       0,   0     },
+    { MOD | SHIFT,      24, /*q*/       killwin,       0,   0     },
+    { MOD | SHIFT,      26, /*e*/       cleanup,       0,   0     },
 };
 
 #endif
