512e13d7b4de1f28dfcf121cef406e5fdcca0f9d
Sina Pinto <sina.pinto@gmail.com>
2015-01-18 18:19:02 -0800

    function name change

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
 #define MOD     XCB_MOD_MASK_1
 #define SHIFT   XCB_MOD_MASK_SHIFT
 
-#define NORMCOLOR    0x002933  /* unfocused window border color */
-#define SELCOLOR     0xafaf87  /* focused window border color */
+#define NORMCOLOR    0xafaf87  /* unfocused window border color */
+#define SELCOLOR     0x2aa198  /* focused window border color */
 #define BORDER_WIDTH 4         /* border width in pixels */
 
 /* find keysyms using `xev` */
@@ -20,7 +20,7 @@ static Key keys[] = {
     { MOD | SHIFT,      45, /*k*/       resize,        0,  -50    },
     { MOD | SHIFT,      43, /*h*/       resize,      -50,   0     },
     { MOD | SHIFT,      46, /*l*/       resize,       50,   0     },
-    { MOD,              38, /*a*/       fullscreen,    0,   0     },
+    { MOD,              38, /*a*/       maximize,      0,   0     },
     { MOD,              23, /*Tab*/     nextwin,       0,   0     },
     { MOD | SHIFT,      24, /*q*/       killwin,       0,   0     },
     { MOD | SHIFT,      26, /*e*/       cleanup,       0,   0     },
