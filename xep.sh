1c44f1b6d7809b51fbf1904227c2d44b1b4bd4b1
Sina Pinto <sina.pinto@gmail.com>
2015-01-27 23:01:38 -0800

    urxvt

--- xep.sh
+++ xep.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# simple Xephyr wrapper for testing a WM in a nested X session
+
 case "$1" in 
-    s) 
+    s|start) 
         [ `pgrep Xephyr` ] && exit 1
         Xephyr -ac -screen 1200x700 :1 &
         sleep 0.5
         DISPLAY=:1
-        urxvt -display :1 -e sh -c "~/.xinitrc aa" &
+        urxvt -display :1 -e sh -c "~/.xinitrc a"
+        urxvt -display :1 &
         ;;
     *) 
         [ `pgrep Xephyr` ] && pkill Xephyr || echo "Xephyr not running"
