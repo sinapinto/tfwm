24194435f6871b5eb909323ddc2720577479b55b
Sina Pinto <sina.pinto@gmail.com>
2015-02-07 10:44:33 -0800

    xephyr being cranky

--- xep.sh
+++ xep.sh
@@ -6,7 +6,7 @@ case "$1" in
     s|start) 
         [ `pgrep Xephyr` ] && exit 1
         Xephyr -ac -screen 1200x700 :1 &
-        sleep 1
+        sleep 0.5
         DISPLAY=:1
         urxvt -display :1 #-e sh -c "~/.xinitrc a"
 #        urxvt -display :1 &
