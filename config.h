97bd150d041cf42e00af043119d8b1b2294d9c76
Sina Pinto <sina.pinto@gmail.com>
2015-02-06 17:36:52 -0800

    keybind documentations

--- config.h
+++ config.h
@@ -22,22 +22,39 @@ static workspace workspaces[5]; /* number of workspaces - if you change this mak
 /* find keycodes using `xev` */
 static key keys[] = {
    /* mod               keycode         function            arg */
+    /* spawn a new terminal (specified in the term[] array above) */
     { MOD ,             XK_Return,      spawn,              {.com=term} },
+    /* move the window down */
     { MOD,              XK_j,           move,               {.i=0}   },
+    /* move the window up */
     { MOD,              XK_k,           move,               {.i=1}   },
+    /* move the window left */
     { MOD,              XK_h,           move,               {.i=2}   },
+    /* move the window right */
     { MOD,              XK_l,           move,               {.i=3}   },
+    /* grow the window vertically */
     { MOD | SHIFT,      XK_j,           resize,             {.i=0}   },
+    /* shrink the window vertically */
     { MOD | SHIFT,      XK_k,           resize,             {.i=1}   },
+    /* shrink the window horizontally */
     { MOD | SHIFT,      XK_h,           resize,             {.i=2}   },
+    /* grow the window horizontally */
     { MOD | SHIFT,      XK_l,           resize,             {.i=3}   },
+    /* grow the window maintaining the aspect ratio */
     { MOD | SHIFT,      XK_m,           resize,             {.i=4}   },
+    /* srhink the window maintaining the aspect ratio */
     { MOD | SHIFT,      XK_n,           resize,             {.i=5}   },
+    /* fullscreen the window (or unfullscreen it) */
     { MOD,              XK_a,           toggle_maximize,    {.i=0}   },
+    /* focus the next window */
     { MOD,              XK_Tab,         nextwin,            {.i=0}   },
+    /* focus the previous window */
     { MOD | SHIFT,      XK_Tab,         nextwin,            {.i=1}   },
+    /* kill the focused window */
     { MOD | SHIFT,      XK_q,           killwin,            {.i=0}   },
+    /* exit bwm */
     { MOD | SHIFT,      XK_e,           cleanup,            {.i=0}   },
+    /* switch to various workspaces */
      DESKTOPCHANGE(     XK_1,                          0)
      DESKTOPCHANGE(     XK_2,                          1)
      DESKTOPCHANGE(     XK_3,                          2)
